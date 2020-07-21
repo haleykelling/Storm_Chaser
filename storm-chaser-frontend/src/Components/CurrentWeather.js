548ee56c7ed63bba43e5f597a0cbeb0776772602
haleykelling <haley@kellings.com>
2020-07-21 15:25:41 -0600

    Show details about weather. Style show page.

--- storm-chaser-frontend/src/Components/CurrentWeather.js
+++ storm-chaser-frontend/src/Components/CurrentWeather.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import '../App.css';
 
-export default function CurrentWeather({ weather }){
-    return(
-        <div className="weather-info">
-            {weather.stormy ? <h3>Risky weather is coming your way!</h3> : <h3>The risks are low right now</h3>}
-            <h4>{weather.cityName}</h4>
-        </div>
-    )
+
+export default function CurrentWeather({ weather, home, backToHome }){
+    const stormy = () => {
+        return weather.weather[0].main === "Thunderstorm" 
+            ? <h3>Risky weather is coming your way!</h3>
+            : <h3>The risks are low right now!</h3>
+    }
+
+    const handleClick = () => {
+        backToHome()
+    }
+    
+    if (!home) {
+        return(
+            <div className="weather-info">
+                {stormy()}
+                <h4 className="city" >{weather["name"]}</h4>
+                <div className="details">
+                    <div className="main-weather">
+                        <p>{weather.weather[0].main}</p>
+                        <img src={"https://openweathermap.org/img/wn/" + weather.weather[0].icon + "@2x.png"} />
+                    </div>
+                    <p><strong>Description:</strong> {weather.weather[0].description}</p>
+                    <p><strong>Current Temp:</strong> {weather.main.temp}<span>&#176;</span> F</p>
+                    <p><strong>Feels Like</strong> {weather.main.feels_like}<span>&#176;</span> F</p>
+                    <p><strong>High:</strong> {weather.main.temp_max}<span>&#176;</span> F</p>
+                    <p><strong>Low:</strong> {weather.main.temp_min}<span>&#176;</span> F</p>
+                    <p><strong>Humidity:</strong> {weather.main.humidity}%</p>
+                    <p><strong>Wind:</strong> {weather.wind.speed} MPH</p>
+                </div>
+                <button onClick={handleClick}>Home Page</button>
+            </div>
+        )
+    } else {
+        return null
+    }
 }
