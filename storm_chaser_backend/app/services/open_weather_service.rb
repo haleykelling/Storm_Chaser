61d54c697bec850b496e165ef77fcbbe030224f5
matthewmjm <matthewmjm@yahoo.com>
2020-07-21 14:04:15 -0600

    change api from forecast to current

--- storm_chaser_backend/app/services/open_weather_service.rb
+++ storm_chaser_backend/app/services/open_weather_service.rb
@@ -1,6 +1,6 @@
 class OpenWeatherService
     def weather_search(zipcode)
-        response = Excon.get("http://api.openweathermap.org/data/2.5/forecast?zip=#{zipcode}&units=imperial&appid=#{ENV['OPEN_WEATHER_API_KEY']}")
+        response = Excon.get("http://api.openweathermap.org/data/2.5/weather?zip=#{zipcode}&units=imperial&appid=#{ENV['OPEN_WEATHER_API_KEY']}")
         byebug
         JSON.parse(response)
     end
